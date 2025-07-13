3e8a0b952ce02fd72465d26c25b6f454c471a319
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-07-14 00:51:08 +0300

    HW_4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,44 +3,41 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
+import Paginate from "../Paginate/Paginate";
 import css from "./App.module.css";
 import toast from "react-hot-toast";
 import type { Movie } from "../../types/movie";
 import { useState } from "react";
+import { useEffect } from "react";
 import { fetchMovies } from "../../services/movieService";
 import { Toaster } from "react-hot-toast";
-
+import { useQuery , keepPreviousData} from "@tanstack/react-query";
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
+  const { data, isError, isLoading, isSuccess } = useQuery({
+    queryKey: ["movies", query, currentPage],
+    queryFn: () => fetchMovies(query, currentPage),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
+ 
 
-
-  const handleSearch = async (searchQuery: string) => {
-
-    try {
-      setIsError(false);
-      setMovies([]);
-      setIsLoading(true);
-      const newMovies = await fetchMovies(searchQuery);
-
-      if (newMovies.length === 0) {
-        toast("No movies found for your request.", {
-          duration: 3000,
-          position: "top-center",
-        });
-      }
-
-      setMovies(newMovies);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = (searchQuery: string) => {
+    setQuery(searchQuery);
+    setCurrentPage(1);
   };
+
+  useEffect(() => {
+    if (data?.results.length === 0) {
+      toast("No movies found for your request");
+    }
+  }, [data]);
+
+  const totalPages = data?.total_pages ?? 0;
 
   const handleSelectedMovie = (movie: Movie) => {
     setSelectedMovie(movie);
@@ -50,23 +47,27 @@ export default function App() {
     setSelectedMovie(null);
   };
 
-
-
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
       <Toaster />
+      {isSuccess && (
+        <Paginate
+          totalPages={totalPages}
+          onChange={setCurrentPage}
+          page={currentPage}
+        />
+      )}
+
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
 
-      {movies.length > 0 && (
-        <MovieGrid onSelect={handleSelectedMovie} movies={movies} />
+      {data && data.results.length > 0 && (
+        <MovieGrid onSelect={handleSelectedMovie} movies={data.results} />
       )}
-      { selectedMovie && (
+      {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={closeModal} />
       )}
     </div>
   );
-};
-
-
+}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,34 +1,31 @@
 import axios from "axios";
-import type {Movie} from "../types/movie"
-
-
-
+import type { Movie } from "../types/movie";
 
 interface FetchMoviesResponse {
   results: Movie[];
+  total_pages: number;
+  page: number;
 }
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<FetchMoviesResponse> => {
   const myKey = import.meta.env.VITE_API_KEY;
-  
+
   axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
-    const response = await axios.get<FetchMoviesResponse>(
-      '/search/movie',
-      {
-        params: {
-          query,
-          page: 1,
-        },
-        headers: {
-          Authorization: `Bearer ${myKey}`,
-          accept: "application/json",
-        },
-      }
-  );
-  
+  const response = await axios.get<FetchMoviesResponse>("/search/movie", {
+    params: {
+      query,
+      page,
+    },
+    headers: {
+      Authorization: `Bearer ${myKey}`,
+      accept: "application/json",
+    },
+  });
+  // console.log( response);
 
-
-    return response.data.results
-
-}
+  return response.data;
+};
